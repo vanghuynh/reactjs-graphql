c521ed38c4c1e29e10db946ab41b7a3e538bbcbb
Vang Huynh <huynh.vang@devexperts.com>
2024-12-01 15:25:42 +0700

    Adding pagination

--- src/pages/Feed/Feed.js
+++ src/pages/Feed/Feed.js
@@ -57,7 +57,7 @@ class Feed extends Component {
     const graphqlQuery = {
       query: `
         {
-          posts {
+          posts(page: ${page}) {
             posts {
               _id
               content
@@ -207,6 +207,7 @@ class Feed extends Component {
             );
             updatedPosts[postIndex] = post;
           } else {
+            updatedPosts.pop();
             updatedPosts.unshift(post);
           }
           return {
